62f81b7546510da11be7147bf2953960021aa55c
cdhxr <althorchxr@gmail.com>
2025-12-08 23:28:40 +0800

    删除postcss文件测试部署

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,vue,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,vue,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,vue,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
